7ec04f971c23c0956c33cf9c36627a6cebd70963
Parwand Alsino <parwand.alsino@hhu.de>
2022-09-02 02:31:11 +0200

    added data to database

---
+++ src/main/resources/db/migration/V2__data.sql
@@ -0,0 +1,4 @@
+INSERT INTO person(firstname, lastname, email, role) VALUES ("parwand", "alsino", "parwand@mail.de", "ROLE_ADMIN");
+INSERT INTO person(firstname, lastname, email, role) VALUES ("user", "alsino", "parwand@mail.de", "ROLE_USER");
+INSERT INTO person(firstname, lastname, email, role) VALUES ("max", "alsino", "parwand@mail.de", "ROLE_USER");
+INSERT INTO person(firstname, lastname, email, role) VALUES ("lea", "alsino", "parwand@mail.de", "ROLE_USER");
